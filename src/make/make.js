6b677f11673eec6d1c65ccb1b30c5e0b42e558c9
김진훈 <kim.jinhoon@nhn.com>
2017-03-17 18:33:50 +0900

    make.js

--- src/make/make.js
+++ src/make/make.js
@@ -1,7 +1,6 @@
 
 const child_process = require( 'child_process' );
 const spawn = child_process.spawn;
-const exec = child_process.exec;
 
 const spawnOption = {
 
@@ -9,9 +8,16 @@ const spawnOption = {
 	stdio: [ 0, 'pipe', 'pipe' ]
 };
 
-const frame = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@';
+const frame = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@';
 
 
+/**
+ * terminal 명령어를 실행한다. 
+ * 1. Promise를 반환하여 비동기 처리를 할 수 있다. 
+ * @param  {[type]} command [description]
+ * @param  {[type]} opt     [description]
+ * @return {[type]}         [description]
+ */
 function terminal( command, opt ){
 	
 	const a = command.split( ' ' ),
@@ -22,19 +28,10 @@ function terminal( command, opt ){
 
 	return new Promise( (resolve, reject) =>{
 
-		child.stdout.on( 'data', data =>{
-
-			console.log( "-", data.toString() );
-		});
-
-		child.stderr.on( 'data', data =>{
-
-			console.log( "x", data.toString() );
-		});
+		child.stdout.on( 'data', data => console.log( "-", data.toString() ) );
+		child.stderr.on( 'data', data => console.log( "x", data.toString() ) );
 
 		child.on( 'close', code =>{
-
-			console.log( 'CLOSE', code );
 
 			if( code == 0 ) return resolve(code);
 
@@ -44,32 +41,74 @@ function terminal( command, opt ){
 }
 
 
-function makeLine( text, char, len ){
 
-	const a = text.split( '' );
+/**
+ * log와 함께 command를 실행한다. 
+ * @param  {[type]} message [description]
+ * @param  {[type]} command [description]
+ * @return {[type]}         [description]
+ */
+function exec( log, command ){
 
-
-}
-
-function alert( message ){
-
-	const char = frame.charAt(0),
-		a = [ frame ];
-
-	a.push( )
-}
-
-function exec( message, command ){
-
-	alert( message );
+	alert( log );
 
 	return terminal( command );
 }
 
 
+terminal( 'npm init' )
+	.then( code => exec( 'Install Webpack', 'npm install webpack --save' ) )
+	.then( code => exec( 'Install Gulp', 'npm install gulp --save' ) )
+	.then( code => console.log( 'COMPLETE', code ) )
+	.catch( code => console.log( 'ERROR', code ) );
 
-// terminal( 'npm init' )
-// 	.then( code => exec( 'Install Webpack', 'npm install webpack --save' ) )
-// 	.then( code => exec( 'Install Gulp', 'npm install gulp --save' ) )
-// 	.then( code => console.log( 'COMPLETE', code ) )
-// 	.catch( code => console.log( 'ERROR', code ) );
+
+
+
+
+//------------------------------------------------------------
+//
+// Utils
+//
+//------------------------------------------------------------
+
+function repeat( char, len ){
+
+	var c = '';
+
+	for( ; len-- ; ) c += char;
+
+	return c;
+}
+
+function makeLine( text, char, len ){
+
+	var s = repeat( ' ', len ).split( '' );
+
+	s[0] = char;
+	s[ s.length - 1 ] = char;
+
+	text = text.slice( 0, len - 2 );
+
+	var start = ( len - text.length ) / 2 | 0;
+
+	for( var i = 0; i < text.length; i++ ){
+		s[ start + i ] = text.charAt(i);
+	}
+
+	return s.join('');
+}
+
+function alert( message ){
+
+	const char = frame.charAt(0),
+		a = [ 
+			frame,
+			makeLine( ' ', char, frame.length ),
+			makeLine( message, char, frame.length ),
+			makeLine( ' ', char, frame.length ),
+			frame,
+		];
+
+	console.log( a.join( '\n' ) );
+}
